750e31136112abdff001e4948d0d0d9739006244
covacialex <96472252+covacialex@users.noreply.github.com>
2021-12-23 11:03:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 "use strict";
 
-const day = document.getElementById("day");
+const day = document.getElementById("profile-timeline-day");
 const week = document.getElementById("profile-timeline-week");
 const month = document.getElementById("profile-timeline-month");
-const trackTimeWork = document.getElementById("track-time-work");
-const trackTimePlay = document.getElementById("track-time-play");
-const trackTimeStudy = document.getElementById("track-time-study");
-const trackTimeExercise = document.getElementById("track-time-exercise");
-const trackTimeSocial = document.getElementById("track-time-social");
-const trackTimeSelfcare = document.getElementById("track-time-selfcare");
-const trackTimelineWork = document.getElementById("track-timeline-work");
-const trackTimelinePlay = document.getElementById("track-timeline-play");
-const trackTimelineStudy = document.getElementById("track-timeline-study");
-const trackTimelineExercise = document.getElementById(
-  "track-timeline-exercise"
-);
-const trackTimelineSocial = document.getElementById("track-timeline-social");
-const trackTimelineSelfcare = document.getElementById(
-  "track-timeline-selfcare"
-);
+
+const timeWork = document.getElementById("time-work");
+const timePlay = document.getElementById("time-play");
+const timeStudy = document.getElementById("time-study");
+const timeExercise = document.getElementById("time-exercise");
+const timeSocial = document.getElementById("time-social");
+const timeSelfcare = document.getElementById("time-selfcare");
+
+const timelinePrev = document.querySelectorAll(".timeline-prev");
+const timelineWork = document.getElementById("timeline-work");
+const timelinePlay = document.getElementById("timeline-play");
+const timelineStudy = document.getElementById("timeline-study");
+const timelineExercise = document.getElementById("timeline-exercise");
+const timelineSocial = document.getElementById("timeline-social");
+const timelineSelfcare = document.getElementById("timeline-selfcare");
 
 let daily = ["5hrs", "1hr", "0hrs", "1hr", "1hr", "0hrs"];
 let dailyPrev = ["7hrs", "2hrs", "1hr", "1hr", "3hrs", "1hr"];
@@ -27,135 +26,57 @@ let weeklyPrev = ["36hrs", "8hrs", "7hrs", "5hrs", "10hrs", "2hrs"];
 let monthly = ["103hrs", "23hrs", "13hrs", "11hrs", "21hrs", "7hrs"];
 let monthlyPrev = ["128hrs", "29hrs", "19hrs", "18hrs", "23hrs", "11hrs"];
 
-day.addEventListener("click", function () {
-  trackTimeWork.textContent = daily[0];
-  trackTimePlay.textContent = daily[1];
-  trackTimeStudy.textContent = daily[2];
-  trackTimeExercise.textContent = daily[3];
-  trackTimeSocial.textContent = daily[4];
-  trackTimeSelfcare.textContent = daily[5];
-});
+console.log(timelinePrev[0]);
 
 day.addEventListener("click", function () {
-  trackTimelineWork.textContent = dailyPrev[0];
-  trackTimelinePlay.textContent = dailyPrev[1];
-  trackTimelineStudy.textContent = dailyPrev[2];
-  trackTimelineExercise.textContent = dailyPrev[3];
-  trackTimelineSocial.textContent = dailyPrev[4];
-  trackTimelineSelfcare.textContent = dailyPrev[5];
+  timelinePrev.forEach((element) => (element.textContent = `Yesterday - `));
+
+  timeWork.textContent = daily[0];
+  timePlay.textContent = daily[1];
+  timeStudy.textContent = daily[2];
+  timeExercise.textContent = daily[3];
+  timeSocial.textContent = daily[4];
+  timeSelfcare.textContent = daily[5];
+  timelineWork.textContent = dailyPrev[0];
+  timelinePlay.textContent = dailyPrev[1];
+  timelineStudy.textContent = dailyPrev[2];
+  timelineExercise.textContent = dailyPrev[3];
+  timelineSocial.textContent = dailyPrev[4];
+  timelineSelfcare.textContent = dailyPrev[5];
 });
 
 week.addEventListener("click", function () {
-  trackTimeWork.textContent = weekly[0];
-  trackTimePlay.textContent = weekly[1];
-  trackTimeStudy.textContent = weekly[2];
-  trackTimeExercise.textContent = weekly[3];
-  trackTimeSocial.textContent = weekly[4];
-  trackTimeSelfcare.textContent = weekly[5];
-});
+  timelinePrev.forEach((element) => (element.textContent = `Last Week - `));
 
-week.addEventListener("click", function () {
-  trackTimelineWork.textContent = weeklyPrev[0];
-  trackTimelinePlay.textContent = weeklyPrev[1];
-  trackTimelineStudy.textContent = weeklyPrev[2];
-  trackTimelineExercise.textContent = weeklyPrev[3];
-  trackTimelineSocial.textContent = weeklyPrev[4];
-  trackTimelineSelfcare.textContent = weeklyPrev[5];
-});
+  timeWork.textContent = weekly[0];
+  timePlay.textContent = weekly[1];
+  timeStudy.textContent = weekly[2];
+  timeExercise.textContent = weekly[3];
+  timeSocial.textContent = weekly[4];
+  timeSelfcare.textContent = weekly[5];
 
-month.addEventListener("click", function () {
-  trackTimeWork.textContent = monthly[0];
-  trackTimePlay.textContent = monthly[1];
-  trackTimeStudy.textContent = monthly[2];
-  trackTimeExercise.textContent = monthly[3];
-  trackTimeSocial.textContent = monthly[4];
-  trackTimeSelfcare.textContent = monthly[5];
+  timelineWork.textContent = weeklyPrev[0];
+  timelinePlay.textContent = weeklyPrev[1];
+  timelineStudy.textContent = weeklyPrev[2];
+  timelineExercise.textContent = weeklyPrev[3];
+  timelineSocial.textContent = weeklyPrev[4];
+  timelineSelfcare.textContent = weeklyPrev[5];
 });
 
 month.addEventListener("click", function () {
-  trackTimelineWork.textContent = monthlyPrev[0];
-  trackTimelinePlay.textContent = monthlyPrev[1];
-  trackTimelineStudy.textContent = monthlyPrev[2];
-  trackTimelineExercise.textContent = monthlyPrev[3];
-  trackTimelineSocial.textContent = monthlyPrev[4];
-  trackTimelineSelfcare.textContent = monthlyPrev[5];
+  timelinePrev.forEach((element) => (element.textContent = `Last Month - `));
+
+  timeWork.textContent = monthly[0];
+  timePlay.textContent = monthly[1];
+  timeStudy.textContent = monthly[2];
+  timeExercise.textContent = monthly[3];
+  timeSocial.textContent = monthly[4];
+  timeSelfcare.textContent = monthly[5];
+
+  timelineWork.textContent = monthlyPrev[0];
+  timelinePlay.textContent = monthlyPrev[1];
+  timelineStudy.textContent = monthlyPrev[2];
+  timelineExercise.textContent = monthlyPrev[3];
+  timelineSocial.textContent = monthlyPrev[4];
+  timelineSelfcare.textContent = monthlyPrev[5];
 });
-
-// console.log(day);
-// Report for Jeremy Robson Daily Weekly Monthly Work 5hrs
-// <!-- daily -->
-// Previous - 7hrs
-// <!-- daily -->
-// 32hrs
-// <!-- weekly -->
-// Previous - 36hrs
-// <!-- weekly -->
-// 103hrs
-// <!-- monthly -->
-// Previous - 128hrs
-// <!-- monthly -->
-
-// Play 1hr
-// <!-- daily -->
-// Previous - 2hrs
-// <!-- daily -->
-// 10hrs
-// <!-- weekly -->
-// Previous - 8hrs
-// <!-- weekly -->
-// 23hrs
-// <!-- monthly -->
-// Previous - 29hrs
-// <!-- monthly -->
-
-// Study 0hrs
-// <!-- daily -->
-// Previous - 1hr
-// <!-- daily -->
-// 4hrs
-// <!-- weekly -->
-// Previous - 7hrs
-// <!-- weekly -->
-// 13hrs
-// <!-- monthly -->
-// Previous - 19hrs
-// <!-- monthly -->
-
-// Exercise 1hr
-// <!-- daily -->
-// Previous - 1hr
-// <!-- daily -->
-// 4hrs
-// <!-- weekly -->
-// Previous - 5hrs
-// <!-- weekly -->
-// 11hrs
-// <!-- monthly -->
-// Previous - 18hrs
-// <!-- monthly -->
-
-// Social 1hr
-// <!-- daily -->
-// Previous - 3hrs
-// <!-- daily -->
-// 5hrs
-// <!-- weekly -->
-// Previous - 10hrs
-// <!-- weekly -->
-// 21hrs
-// <!-- monthly -->
-// Previous - 23hrs
-// <!-- monthly -->
-
-// Self Care 0hrs
-// <!-- daily -->
-// Previous - 1hr
-// <!-- daily -->
-// 2hrs
-// <!-- weekly -->
-// Previous - 2hrs
-// <!-- weekly -->
-// 7hrs
-// <!-- monthly -->
-// Previous - 11hrs
-// <!-- monthly -->
